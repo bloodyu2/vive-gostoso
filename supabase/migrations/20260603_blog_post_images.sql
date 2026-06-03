2b98ad29aae16e4e6224285ff4f9331780428995
Victor Lima <victor.lima@balaio.net>
2026-06-03 13:40:22 -0300

    feat(seo): add blog post cover images + Google Search Console verification file

---
+++ supabase/migrations/20260603_blog_post_images.sql
@@ -0,0 +1,6 @@
+-- Add cover images to blog posts
+update gostoso_blog_posts set cover_url = '/images/blog/como-chegar.jpg' where slug = 'como-chegar-sao-miguel-do-gostoso';
+update gostoso_blog_posts set cover_url = '/images/blog/kitesurf.jpg' where slug = 'kitesurf-sao-miguel-do-gostoso';
+update gostoso_blog_posts set cover_url = '/images/blog/o-que-fazer.jpg' where slug = 'o-que-fazer-sao-miguel-do-gostoso';
+update gostoso_blog_posts set cover_url = '/images/blog/praias.jpg' where slug = 'praias-sao-miguel-do-gostoso';
+update gostoso_blog_posts set cover_url = '/images/blog/pousadas.jpg' where slug = 'pousadas-sao-miguel-do-gostoso';
